b6ab729143def09374a097a96ac9af964588332a
fujitoshion <shion@shion.local>
2020-11-10 15:52:33 +0900

    weathe.py

--- weather1.py
+++ weather1.py
@@ -9,3 +9,32 @@ sm = QtScxml.QScxmlStateMachine.fromFile('states.scxml')
 
 sm.start()
 el.processEvents()
+
+print('SYS> こんにちは天気情報システムです')
+
+uttdic ={'ask_place':'地名を言ってください',
+        'ask_date':'日付を言ってください',
+        'ask_type':"情報種別は"}
+
+
+current_state = sm.activeStateNames()[0]
+print("currnt_state=", current_state)
+
+sysutt = uttdic[current_state]
+print('SYS>', sysutt)
+
+while True:
+    text = input('> ')
+    sm.submitEvent(text)
+    el.processEvents()
+
+    current_state = sm.activeStateNames()[0]
+    print("currnt_state=", current_state)
+
+    if current_state == 'tell_info':
+        print('お天気をお伝えします')
+        break
+    else:
+        sysutt = uttdic[current_state]
+        print('SYS>', sysutt)
+print('THank you')
